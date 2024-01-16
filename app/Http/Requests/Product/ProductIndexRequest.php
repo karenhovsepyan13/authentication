c6940ae7d3015f0ebc3c6ed2cf6a7df1e6451e29
Karen <karen.hovsepyan.130@gmail.com>
2024-01-17 00:50:09 +0400

    First commit

---
+++ app/Http/Requests/Product/ProductIndexRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductIndexRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function getUserId(): int
+    {
+        $user = Auth::user();
+         return $user->id;
+    }
+}
